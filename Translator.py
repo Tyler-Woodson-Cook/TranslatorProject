a701b0dc35583f568120d354d5b643bc4deb8608
Tyler Cook <tycook110@gmail.com>
2024-05-18 23:32:46 +0200

    Trying to get the dataprocessor working currently so I added that and hparams.

--- Translator.py
+++ Translator.py
@@ -32,3 +32,45 @@ wmt_transformer_model, wmt_src_vocab, wmt_tgt_vocab = \
 
 # we are using a model that has a mixed vocab of EN-DE, so the source and target language vocab are the same
 print(len(wmt_src_vocab), len(wmt_tgt_vocab))
+
+
+# processing steps 
+# clip the source and target sequences via nlp.data.WMT2014BPE
+# split the string input to a list of tokens
+# map the string token into its index in the vocabulary
+# append EOS token to source sentence and add BOS and EOS tokens to target sentence.
+import hyperparameters as hparams
+
+
+wmt_data_test = nlp.data.WMT2014BPE('newstest2014', src_lang=hparams.src_lang, tgt_lang=hparams.tgt_lang)
+print('Source language %s, Target language %s' % (hparams.src_lang, hparams.tgt_lang))
+print('Sample BPE tokens: "{}"'.format(wmt_data_test[0]))
+
+wmt_test_text = nlp.data.WMT2014('newstest2014',src_lang=hparams.src_lang, tgt_lang=hparams.tgt_lang)
+print('Sample raw text: "{}"'.format(wmt_test_text[0]))
+
+wmt_test_tgt_sentences = wmt_test_text.transform(lambda src, tgt: tgt)
+print('Sample target sentence: "{}"'.format(wmt_test_tgt_sentences[0]))
+
+
+
+import dataprocessor
+
+print(dataprocessor.TrainValDataTransform.__doc__)
+
+# wmt_transform_fn includes the four preprocessing steps mentioned above.
+wmt_transform_fn = dataprocessor.TrainValDataTransform(wmt_src_vocab, wmt_tgt_vocab)
+wmt_dataset_processed = wmt_data_test.transform(wmt_transform_fn, lazy=False)
+print(*wmt_dataset_processed[0], sep='\n')
+
+def get_length_index_fn():
+    global idx
+    idx = 0
+    def transform(src, tgt):
+        global idx
+        result = (src, tgt, len(src), len(tgt), idx)
+        idx += 1
+        return result
+    return transform
+
+wmt_data_test_with_len = wmt_dataset_processed.transform(get_length_index_fn(), lazy=False)
